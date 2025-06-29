494ae9b25cd5f9d5c0653aa6b9cac6d0ec6042bb
KaiEngelmann <KaiVaughnMusic@Yahoo.com>
2025-06-29 17:58:59 -0400

    created circle shape and FPS

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import pygame 
 from constants import *
+from circleshape import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    # creates object for FPS
+    fps = pygame.time.Clock()
+    dt = 0
+
+    # creates game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        # dt limits loop to 60 FPS
+
+        dt = fps.tick(60) / 1000
+
     print(f"Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
