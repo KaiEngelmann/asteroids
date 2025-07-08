1ba0e4d8810928fb6741f35b91ffe2e82526cb6e
KaiEngelmann <KaiVaughnMusic@Yahoo.com>
2025-07-08 15:00:10 -0400

    added score and lives to screen

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ def main():
     # creates object for FPS
     fps = pygame.time.Clock()
     dt = 0
+    pygame.font.init()
+    font = pygame.font.SysFont("Arial", 24)
 
     # create groups for game objects
     updatable = pygame.sprite.Group()
@@ -35,25 +37,49 @@ def main():
 
 
     asteroid_field = AsteroidField()
-
+    hud_color = (225, 225, 255)
+    score = 0
+    lives = 3
+    time_since_last_hit = 999
+    invincibility_duration = 2 
     # creates game loop
     while True:
+        time_since_last_hit += dt
+        player_hit = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
         updatable.update(dt)
-        for sprite in asteroid_group:
-            if sprite.collisions(player) == True:
-                print("Game Over!")
-                sys.exit()
+        if time_since_last_hit > invincibility_duration:
+            for sprite in asteroid_group:
+                if sprite.collisions(player) == True:
+                    player_hit = True
+                    break
+        if player_hit:
+            lives -= 1
+            time_since_last_hit = 0
+            player.kill()
+            player = Player(x, y)
+            print(f"{lives} Lives Remaining")
+
+        if lives <= 0:
+            print("Game Over!")
+            sys.exit()
         for asteroid in asteroid_group:
             for shot in shot_group:
                 if asteroid.collisions(shot) == True:
                     asteroid.split()
                     shot.kill()
+                    score += 1
         for sprite in drawable:
             sprite.draw(screen)
+
+        lives_text = font.render(f"Lives: {lives}", True, hud_color)
+        score_text = font.render(f"Score: {score}", True, hud_color)
+        
+        screen.blit(lives_text, (10, 10))
+        screen.blit(score_text, (SCREEN_WIDTH - score_text.get_width() -10, 10))
         pygame.display.flip()
         # dt limits loop to 60 FPS
 
