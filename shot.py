e684aaef305d4acb719b14c37d05d4a42e161a2f
Jasmine <156473751+JasmineH18@users.noreply.github.com>
2025-07-12 19:11:43 -0400

    Made shots look better!

--- shot.py
+++ shot.py
@@ -7,10 +7,12 @@ class Shot(CircleShape):
         super().__init__(x, y, SHOT_RADIUS)
         self.velocity = pygame.Vector2(velocity)
     def draw(self, screen):
-        color = (255, 255, 255)
-        center = (self.position.x, self.position.y)
+        outer_color = (255, 140, 0)  # orange
+        inner_color = (255, 255, 0)  # yellow
+        center = (int(self.position.x), int(self.position.y))
+        pygame.draw.circle(screen, outer_color, center, self.radius)
+        pygame.draw.circle(screen, inner_color, center, self.radius // 2)
 
-        pygame.draw.circle(screen, color, center, self.radius, width=1)
 
     def update(self, dt):
             
