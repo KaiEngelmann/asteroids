bbb40e90087a1f207113c0331e78f4078ddc9cbb
Jasmine <156473751+JasmineH18@users.noreply.github.com>
2025-07-12 18:58:18 -0400

    added graphics to asteroids

--- asteroids.py
+++ asteroids.py
@@ -1,16 +1,30 @@
 from circleshape import *
 from constants import *
 import random
+import os
+import pygame
+
+ASTEROID_IMAGES = []
+def load_asteroid_images():
+    global ASTEROID_IMAGES
+    paths = [f"D:/Python Projects/asteroids/asteroid_{i}.png" for i in range(2, 5)]
+    ASTEROID_IMAGES = [pygame.image.load(p).convert_alpha() for p in paths]
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius, round_number=1):
         super().__init__(x, y, radius)
         self.round_number = round_number
-    def draw(self, screen):
-        color = (255, 255, 255)
-        center = (self.position.x, self.position.y)
+        self.image = random.choice(ASTEROID_IMAGES)
+        self.update_scaled_image()
 
-        pygame.draw.circle(screen, color, center, self.radius, width=2)
+    def update_scaled_image(self):
+        # Scale the image to fit the radius (diameter)
+        diameter = self.radius * 2
+        self.scaled_image = pygame.transform.smoothscale(self.image, (int(diameter), int(diameter)))
+
+    def draw(self, screen):
+        rect = self.scaled_image.get_rect(center=(self.position.x, self.position.y))
+        screen.blit(self.scaled_image, rect)
 
     def update(self, dt):
             
@@ -29,4 +43,3 @@ class Asteroid(CircleShape):
         speed_multiplier = 1.2
         asteroid1.velocity = vector1 * speed_multiplier
         asteroid2.velocity = vector2 * speed_multiplier
-
